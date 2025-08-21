93017a8f94c90a940b9b4b47ac1b48d5583298bb
qvink <qvink@users.noreply.github.com>
2025-08-21 16:40:53 -0600

    fixing some interface buttons not wrapping properly on small screens

--- index.js
+++ index.js
@@ -1754,13 +1754,13 @@ class MemoryEditInterface {
 <hr>
 <div>Bulk Actions (Selected: <span id="selected_count"></span>)</div>
 <div id="bulk_actions" class="flex-container justifyspacebetween alignitemscenter">
-    <button id="bulk_remember"   class="menu_button flex1" title="Toggle inclusion of selected summaries in long-term memory"> <i class="fa-solid fa-brain"></i>Remember</button>
-    <button id="bulk_exclude"    class="menu_button flex1" title="Toggle inclusion of selected summaries from all memory">     <i class="fa-solid fa-ban"></i>Exclude</button>
-    <button id="bulk_copy"       class="menu_button flex1" title="Copy selected memories to clipboard">                        <i class="fa-solid fa-copy"></i>Copy</button>
-    <button id="bulk_summarize"  class="menu_button flex1" title="Re-Summarize selected memories">                             <i class="fa-solid fa-quote-left"></i>Summarize</button>
-    <button id="bulk_delete"     class="menu_button flex1" title="Delete selected memories">                                   <i class="fa-solid fa-trash"></i>Delete</button>
-    <button id="bulk_regex"      class="menu_button flex1" title="Run the selected regex script on selected memories">         <i class="fa-solid fa-shuffle"></i>Regex Replace</button>
-    <select id="regex_selector"  title="Choose regex script"></select>
+    <div class="flex1"><button id="bulk_remember"   class="menu_button" title="Toggle inclusion of selected summaries in long-term memory"> <i class="fa-solid fa-brain"></i>Remember</button></div>
+    <div class="flex1"><button id="bulk_exclude"    class="menu_button" title="Toggle inclusion of selected summaries from all memory">     <i class="fa-solid fa-ban"></i>Exclude</button></div>
+    <div class="flex1"><button id="bulk_copy"       class="menu_button" title="Copy selected memories to clipboard">                        <i class="fa-solid fa-copy"></i>Copy</button></div>
+    <div class="flex1"><button id="bulk_summarize"  class="menu_button" title="Re-Summarize selected memories">                             <i class="fa-solid fa-quote-left"></i>Summarize</button></div>
+    <div class="flex1"><button id="bulk_delete"     class="menu_button" title="Delete selected memories">                                   <i class="fa-solid fa-trash"></i>Delete</button></div>
+    <div class="flex1"><button id="bulk_regex"      class="menu_button" title="Run the selected regex script on selected memories">         <i class="fa-solid fa-shuffle"></i>Regex Replace</button></div>
+    <div class="flex1"><select id="regex_selector"  title="Choose regex script"></select>
 </div>
 </div>
 `
